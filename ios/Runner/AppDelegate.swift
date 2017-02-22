0f11266eb162314e3c33b5feb8b584d642f07072
Chinmay Garde <chinmaygarde@google.com>
2017-02-22 14:18:17 -0800

    Replace Objective-C embedder with Swift implementation.

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+// Copyright 2017 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+  {
+    return true
+  }
+}
